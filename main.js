dc409403bd076c9a045dd40976e0d88a30cc98a9
Maftuna <156097566+maftunarozimatova@users.noreply.github.com>
2024-03-15 23:04:18 +0500

    for

---
+++ main.js
@@ -0,0 +1,25 @@
+let array = [4,6,19,22,11,144,967,19124]
+array.sort()
+console.log(array);
+
+for (let i = 0; i < array.length; i++) {
+    if (i % 2 === 0) {
+        alert( i )
+        if (array == true) {
+            
+        } 
+        }
+        array.push[i]
+    }
+
+    console.log(array);
+
+for (let i = 0; i < array.length; i++) {
+    if (i % 2 === 0) {
+        alert( i )
+        if (array == true) {
+        }
+        array.push[i]
+    }
+    console.log(array);
+}
